24a95af4e762752c01f5c21deed7e2dac0c5a2fa
Jerred Shepherd <shepherdjerred@gmail.com>
2020-07-15 21:40:43 -0700

    Add naive searching

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import data from "../data/dump.json";
 import {Parser} from "../parser/Parser";
 import {Content} from "../model/Content";
@@ -6,9 +6,13 @@ import {Container} from "./Container";
 import {CourseList} from "./CourseList";
 import {Hero} from "./Hero";
 import {FilterBar} from "./FilterBar";
+import {Course} from "../model/Course";
+import Fuse from "fuse.js";
 
 export interface AppState {
   data: Content;
+  search: string;
+  fuse: Fuse<Course, Fuse.IFuseOptions<Course>>
 }
 
 export default class App extends React.Component<unknown, AppState> {
@@ -16,19 +20,56 @@ export default class App extends React.Component<unknown, AppState> {
     super(props);
 
     const parser = new Parser();
+    const content = parser.parse(JSON.stringify(data));
+
+    console.log(content);
+
+    const options = {
+      keys: [
+        "title",
+        "description",
+        "videos.video.title",
+        "videos.video.description"
+      ]
+    }
+
+    const index = Fuse.createIndex(options.keys, content.courses)
 
     this.state = {
-      data: parser.parse(JSON.stringify(data)),
+      data: content,
+      fuse: new Fuse<Course, Fuse.IFuseOptions<Course>>(content.courses, options, index),
+      search: ""
     };
   }
 
+  onFilter(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      ...this.state,
+      search: event.target.value || ""
+    })
+  }
+
+  findCourses(): Course[] {
+    const allCourses = this.state.data.courses;
+    const query = this.state.search;
+    const fuse = this.state.fuse;
+
+    if (query === "") {
+      return allCourses;
+    } else {
+      return fuse.search(query).map((result) => result.item);
+    }
+  }
+
   render() {
+    const courses = this.findCourses();
+
     return (
         <React.Fragment>
           <Hero title="Courses" subtitle=""/>
           <Container>
-            <FilterBar/>
-            <CourseList courses={this.state.data.courses}/>
+            <FilterBar onUpdate={this.onFilter.bind(this)}/>
+            <CourseList courses={courses}/>
           </Container>
         </React.Fragment>
     );
--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 
-export class FilterBar extends React.Component<any, any> {
+export interface FilterBarProps {
+  onUpdate: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface FilterBarState {
+  value: string;
+}
+
+export class FilterBar extends React.Component<FilterBarProps, FilterBarState> {
+  constructor(props: FilterBarProps) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
+  onUpdate(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      ...this.state,
+      value: event.target.value || ""
+    });
+    this.props.onUpdate(event);
+  }
+
   render() {
     return (
-        <nav className="panel">
-          <p className="panel-heading">
-            Filter
-          </p>
-          <div className="panel-block"/>
-        </nav>
+        <div>
+          <input type="text" value={this.state.value} onChange={this.onUpdate.bind(this)}/>
+        </div>
     );
   }
 }
