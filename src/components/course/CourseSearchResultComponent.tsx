a97250df8d2e60b46df2704245a8a1935e9f1915
Jerred Shepherd <shepherdjerred@gmail.com>
2020-07-18 16:54:57 -0700

    Update data

--- src/components/course/CourseSearchResultComponent.tsx
+++ src/components/course/CourseSearchResultComponent.tsx
@@ -1,8 +1,8 @@
-import {Course} from "../../model/Course";
-import {getCourseUrl, getVideoUrl} from "../../utils/UrlUtilities";
+import { Course } from "../../model/Course";
+import { getCourseUrl, getVideoUrl } from "../../utils/UrlUtilities";
 import React from "react";
 import Highlighter from "react-highlight-words";
-import "./CourseSearchResultComponent.css"
+import "./CourseSearchResultComponent.css";
 
 export interface CourseSearchResult {
   course: Course;
@@ -17,38 +17,34 @@ export function CourseSearchResultComponent(props: CourseSearchResultProps) {
   const course = props.result.course;
 
   let videos = course.videos
-  .map((video) => {
-    return {
-      name: video.video.title,
-      uuid: video.video.uuid,
-      link: getVideoUrl(video.video, getCourseUrl(course)),
-    };
-  })
-  .map((video) => {
-    return (
+    .map((video) => {
+      return {
+        name: video.video.title,
+        uuid: video.video.uuid,
+        link: getVideoUrl(video.video, getCourseUrl(course)),
+      };
+    })
+    .map((video) => {
+      return (
         <li key={video.uuid}>
           <a href={video.link}>
-            <Highlighter searchWords={props.result.matches} textToHighlight={video.name}
-                         autoEscape={true}/>
+            <Highlighter searchWords={props.result.matches} textToHighlight={video.name} autoEscape={true} />
           </a>
         </li>
-    );
-  });
+      );
+    });
 
   return (
-      <div key={course.uuid} className="box">
-        <div className="box-content">
-          <h3 className="title">
-            <Highlighter searchWords={props.result.matches} textToHighlight={course.title}
-                         autoEscape={true}/>
-          </h3>
-          <p>
-            {course.description}
-          </p>
-          <p>
-            <ol>{videos}</ol>
-          </p>
-        </div>
+    <div key={course.uuid} className="box">
+      <div className="box-content">
+        <h3 className="title">
+          <Highlighter searchWords={props.result.matches} textToHighlight={course.title} autoEscape={true} />
+        </h3>
+        <p>{course.description}</p>
+        <p>
+          <ol>{videos}</ol>
+        </p>
       </div>
+    </div>
   );
 }
