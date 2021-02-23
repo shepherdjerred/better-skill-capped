d192f3d074a8799908cc8d45d2c52fea403a8c49
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-02-23 15:27:02 -0800

    Add Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,18 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "./bulma.sass";
 import * as Sentry from "@sentry/react";
+import { GA4React } from "ga-4-react";
+import { GA4ReactResolveInterface } from "ga-4-react/dist/models/gtagModels";
+
+const ga4react = new GA4React("G-JPTQRHF6LZ");
+ga4react.initialize().then(
+  (ga4: GA4ReactResolveInterface) => {
+    ga4.pageview(window.location);
+  },
+  (err) => {
+    console.error(err);
+  }
+);
 
 Sentry.init({
   dsn: "https://7822525c2ffb4c61a436c1dfdfa14be8@o92742.ingest.sentry.io/5364733",
