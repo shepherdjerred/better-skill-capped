cf226a4aaa9cca58d9208e20b3cdafce5a40348a
Jerred Shepherd <github@sjer.red>
2023-10-07 11:38:22 -0700

    use firebase to fetch

--- fetcher/src/worker.ts
+++ fetcher/src/worker.ts
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
+
 export interface Env {
   // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
   BUCKET: R2Bucket;
@@ -5,18 +8,27 @@ export interface Env {
 
 export default {
   async scheduled(_event: ScheduledEvent, env: Env, _ctx: ExecutionContext): Promise<void> {
-    const response = await fetch("https://www.skill-capped.com/lol/browse");
-    const body = await response.text();
-    const groups = body.match(/loc: *"(.*)"/);
-    if (groups === null || groups[1] === undefined) {
-      console.error(body, groups);
-      throw Error("unable to find url");
-    }
-    const url = groups[1];
+    const firebaseConfig = {
+      apiKey: "AIzaSyAgHWuN2OEx5R827dHlKO9HsOuBwZ017n0",
+      authDomain: "sc-site-a8f24.firebaseapp.com",
+      databaseURL: "https://sc-site-a8f24.firebaseio.com",
+      projectId: "sc-site-a8f24",
+      storageBucket: "sc-site-a8f24.appspot.com",
+      messagingSenderId: "385410121336",
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    const db = getFirestore(app);
+    const docRef = await doc(db, "content-location/lol-content-location");
+    const docSnap = await getDoc(docRef);
+    const url: string = docSnap.data().dumpUrl
+
     console.log(`found ${url}`);
     const jsonResponse = await fetch(url);
     const jsonContent = await jsonResponse.json();
     console.log(jsonContent);
+
     await env.BUCKET.put("manifest.json", JSON.stringify(jsonContent));
     console.log("all done!");
   },
